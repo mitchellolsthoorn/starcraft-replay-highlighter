5fd797dce5d6156dca764c5aca638e0b0b70cb66
Jean de Leeuw <jeandeleeuw@gmail.com>
2017-07-01 14:58:22 +0200

    More changes to how highlights are selected. Old code is still in

--- starcraft/analyser.py
+++ starcraft/analyser.py
@@ -29,7 +29,7 @@ FRAME_DURATION = 0.042
 BOT_THRESHOLD = 400
 DISTANCE_THRESHOLD = 100
 COMPRESSION_LENGTH = 2 #in windows, highlights that are within COMPRESSION_LENGTH windows of eachother will be merged.
-
+ATTACK_COMPRESSION = 2 #in windows
 
 def process_actions(action_data):
     game_data = {0: dict(), 1: dict()}
@@ -227,48 +227,61 @@ def make_location_graph(player1_x_data, player1_y_data, player2_x_data, player2_
 
 
 def formatTime(seconds):
-	minutes = int(seconds / 60)
-	sec = int(seconds % 60) 
-	if sec < 10:
-		sec = '0' + str(sec)
-	return '{minutes}:{seconds}'.format(minutes=minutes, seconds=sec)
+    minutes = int(seconds / 60)
+    sec = int(seconds % 60) 
+    if sec < 10:
+        sec = '0' + str(sec)
+    return '{minutes}:{seconds}'.format(minutes=minutes, seconds=sec)
 
 def attack_highlights(attacks):
-	average_attack_amount = 0
+    average_attack_amount = 0
 
-	for amount, frame in attacks:
-		average_attack_amount += amount
+    for amount, frame in attacks:
+        average_attack_amount += amount
 
-	average_attack_amount = average_attack_amount / len(attacks)
+    average_attack_amount = average_attack_amount / len(attacks)
 
-	return filter(lambda x: x[0] > average_attack_amount, attacks)
+    return filter(lambda x: x[0] > average_attack_amount, attacks)
 
 def extract_duplicates(windows):
-	seen = set()
-	duplicates = dict()
-	for x in windows:
-		if x in seen:
-			if x in duplicates:
-				duplicates[x] += 1
-			else:
-				duplicates[x] = 1
-		else:
-			seen.add(x)
-	return sorted(duplicates.items())
+    seen = set()
+    duplicates = dict()
+    for x in windows:
+        if x in seen:
+            if x in duplicates:
+                duplicates[x] += 1
+            else:
+                duplicates[x] = 1
+        else:
+            seen.add(x)
+
+    return sorted(duplicates.items())
 
 def compress_highlights(highlights):
-	delete = []
+    delete = []
 
+    for i, val in enumerate(highlights[:-1]):
+        if highlights[i+1] - highlights[i] <= COMPRESSION_LENGTH:
+            highlights[i+1] = int( (highlights[i+1] + highlights[i]) / 2)
+            delete.append(i)
 
-	for i, val in enumerate(highlights[:-1]):
-		if highlights[i+1] - highlights[i] <= COMPRESSION_LENGTH:
-			highlights[i+1] = int( (highlights[i+1] + highlights[i]) / 2)
-			delete.append(i)
+    for i in reversed(delete):
+        del highlights[i]
 
-	for i in reversed(delete):
-		del highlights[i]
+    return highlights
 
-	return highlights
+def compress_attacks(attacks):
+    delete = []
+
+    for i, val in enumerate(attacks[:-1]):
+        if attacks[i+1][1] - attacks[i][1] <= ATTACK_COMPRESSION:
+            attacks[i+1] = ( attacks[i+1][0] + attacks[i][0], int( (attacks[i+1][1] + attacks[i][1]) / 2) )
+            delete.append(i)
+
+    for i in reversed(delete):
+        del attacks[i]
+
+    return attacks
 
 
 def main():
@@ -333,23 +346,41 @@ def main():
     p1_apm_windows = player1.max[1]
     p2_apm_windows = player2.max[1]
 
+    p1_compressed_attacks = compress_attacks(player1_attack)
+    p2_compressed_attacks = compress_attacks(player2_attack)
+
+    _, p1_attack_windows = zip(*p1_compressed_attacks)
+    _, p2_attack_windows = zip(*p2_compressed_attacks)
+
+    #print 'attacks, ' , p2_attack_windows
+
+    #print 'p1 attack:' , player1_attack, ' --- ', len(player1_attack)
+    #print 'p1 attack compressed', compress_highlights(player1_attack_windows), '----', len(compress_highlights(player1_attack_windows))
+
     #_, p1_attack_windows = zip(*attack_highlights(player1_attack))
     #_, p2_attack_windows = zip(*attack_highlights(player2_attack))
-    p1_attack_windows = player1_attack_windows
-    p2_attack_windows = player2_attack_windows
+    #p1_attack_windows = player1_attack_windows
+    #p2_attack_windows = player2_attack_windows
 
     common_windows = common_windows
 
-    potential_interest = [p1_apm_windows] + [p2_apm_windows] + list(p1_attack_windows) + list(player2_attack_windows) + common_windows
-    interest = extract_duplicates(potential_interest)
+    potential_interest_windows = [p1_apm_windows] + [p2_apm_windows] + list(p1_attack_windows) + list(player2_attack_windows) + common_windows
+    potential_interest_windows = sorted(potential_interest_windows)
+    #interest = extract_duplicates(potential_interest)
 
-    if(len(interest) <= 3):
-    	logging.info('Too few actual highlights. Added the potential highlights.')
-    	windows = sorted(potential_interest)
-    else:
-    	windows, _ = zip(*interest)
+    #if(len(interest) <= 3):
+    #   logging.info('Too few actual highlights. Added the potential highlights.')
+    #   windows = sorted(potential_interest)
+    #else:
+    #   windows, _ = zip(*interest)
 
-    windows = compress_highlights(list(windows))
+    #windows, x = zip(*interest)
+    #print windows
+    #print x
+    #windows, _ = zip(*interest)
+
+    windows = compress_highlights(potential_interest_windows)
+
 
     print 'Highlighting windows: ' + str(windows)
     print 'Highlighting times: ' + str(map(formatTime, map(lambda x: x * FRAME_DURATION * WINDOW_SIZE, windows)))
